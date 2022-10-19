2462b7a551e3d2522cb4568d280c0412f7f38100
iolowookere217 <olowookere.isaaco@gmail.com>
2022-10-18 20:54:47 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,8 @@
 /**
  * main - a program that prints _putchar followed by a new line
  *
- * Return - Always 0
+ * Return - Always 0 (success)
  */
-
 int main(void)
 {
 	_putchar('_');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-/*
- * file - main.h
- * description - header file containing declarations for all functions used
- */
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
