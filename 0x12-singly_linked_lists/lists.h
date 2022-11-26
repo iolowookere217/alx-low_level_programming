0907d14c7daeabb70dc6b34e1667fdf8eeb07877
iolowookere217 <olowookere.isaaco@gmail.com>
2022-11-25 19:26:16 -0800

    Task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,29 +1,35 @@
-#include "lists.h"
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 
-/**
- * * print_list - Prints all the elements of a list_t list.
- * * @h: The list_t list.
- * *
- * * Return: The number of nodes in h.
+/*
+ * * File: lists.h
+ * * Auth: Isaac O
+ * * Desc: Header file containing prototypes and definitions for all
+ * * functions and types written in the 0x11-singly_linked_lists directory.
  */
 
-size_t print_list(const list_t *h)
+#include <stdlib.h>
+
+/**
+ * * struct list_s - singly linked list
+ * * @str: string - (malloc'ed string)
+ * * @len: length of the string
+ * * @next: points to the next node
+ * *
+ * * Description: singly linked list node structure for Alx  project
+ */
+typedef struct list_s
 {
-	size_t nodes = 0;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
-}
+#endif /* LISTS_H */
 
