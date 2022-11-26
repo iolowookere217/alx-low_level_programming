1450b8c97c4c3d4bc26ea08d0b3d5f5af7b70039
iolowookere217 <olowookere.isaaco@gmail.com>
2022-11-25 20:06:33 -0800

    Task 2-4

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>
+
+/*
+ * * File: lists.h
+ * * Auth: Isaac O
+ * * Desc: Header file containing prototypes and definitions for all functions
+ * *       and types written in the 0x11-singly_linked_lists directory.
+ */
+
+#include <stdlib.h>
+
 /**
  * * struct list_s - singly linked list
  * * @str: string - (malloc'ed string)
@@ -8,6 +17,7 @@
  * * @next: points to the next node
  * *
  * * Description: singly linked list node structure
+ * *              for Alx project
  */
 typedef struct list_s
 {
@@ -16,12 +26,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
 size_t print_list(const list_t *h);
-
 size_t list_len(const list_t *h);
-
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
 
